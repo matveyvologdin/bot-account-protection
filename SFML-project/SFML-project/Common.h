1ad3f0dc81f8164878e194d5e46422fcaaa3cca6
Ping_Win <47752249+matveyvologdin@users.noreply.github.com>
2022-07-13 22:47:11 +0300

    Delete SFML-project directory

--- SFML-project/SFML-project/Common.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-const char* dbPath = "Users.txt";
-const char* loginAlphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
-const char* passowrdAlphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
-const char* btnDownloadsTitle = "D:\\SFML-project\\SFML-project\\SFML-project\\images\\111.png";
-
-
